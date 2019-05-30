1636d929d146833fceaaa163de250748d3402761
ZCreturn0 <237350543@qq.com>
2019-05-30 23:56:44 +0800

    地图定位

--- hello_world/pages/logs/logs.js
+++ hello_world/pages/logs/logs.js
@@ -3,13 +3,27 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    logs: [],
+    lat: 39.905348414898995,
+    lng: 116.40717000000001
   },
   onLoad: function () {
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
         return util.formatTime(new Date(log))
       })
+    })
+  },
+  locate: function(){
+    var _this = this;
+    wx.getLocation({
+      type: 'wgs84',
+      success: function(res) {
+        _this.setData({
+          lat: res.latitude,
+          lng: res.longitude
+        });
+      }
     })
   }
 })
