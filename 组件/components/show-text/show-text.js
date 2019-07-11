2900c07b57e02fe0b64f1a7718d2fc64a6afd8c1
ZCreturn0 <237350543@qq.com>
2019-07-11 22:38:18 +0800

    自定义组件

---
+++ 组件/components/show-text/show-text.js
@@ -0,0 +1,26 @@
+// components/show-text/show-text.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    text: {
+      type: String,
+      value: 'aaaaa'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    innerData: 'aaaa'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
