182e2a4fb02c8d2d86e3165def1f660d463c893e
ZCreturn0 <237350543@qq.com>
2019-07-14 23:04:34 +0800

    父组件

---
+++ 组件/components/list-ul/list-ul.js
@@ -0,0 +1,43 @@
+// components/list-ul/list-ul.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  },
+  /**
+   *  关系
+   */
+  relations: {
+    '../list-li/list-li': {
+      type: 'child',
+      // 插入,target是该节点实例对象，触发在该节点attached生命周期之后
+      linked: function(target){
+        console.log('one child node added');
+      },
+      // 移动
+      linkChanged: function(target){
+        console.log('one child node moved');
+      },
+      // 删除
+      unlinked: function(target){
+        console.log('one child node removed');
+      }
+    }
+  }
+})
